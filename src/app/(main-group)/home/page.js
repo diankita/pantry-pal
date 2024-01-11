c1128bd61a30648a1e63b11ffdfe25874f478cad
Joel Cheah <gjoelcheah@gmail.com>
2024-01-11 16:20:15 +0000

    Add placeholder content for home

---
+++ src/app/(main-group)/home/page.js
@@ -0,0 +1,40 @@
+import { Button } from '@mui/material';
+import styles from './page.module.css';
+
+export default function Page() {
+  return (
+    <>
+      <h1>this is home</h1>
+      <Button variant="contained">Contained</Button>
+      <div>asdfsdfa</div>
+      <div>asdfsdfa</div>
+      <div>asdfsdfa</div>
+      <div>asdfsdfa</div>
+      <div>asdfsdfa</div>
+      <div>asdfsdfa</div>
+      <div>asdfsdfa</div>
+      <div>asdfsdfa</div>
+      <div>asdfsdfa</div>
+      <div>asdfsdfa</div>
+      <div>asdfsdfa</div>
+      <div>asdfsdfa</div>
+      <div>asdfsdfa</div>
+      <div>asdfsdfa</div>
+      <div>asdfsdfa</div>
+      <div>asdfsdfa</div>
+      <div>asdfsdfa</div>
+      <div>asdfsdfa</div>
+      <div>asdfsdfa</div>
+      <div>asdfsdfa</div>
+      <div>asdfsdfa</div>
+      <div>asdfsdfa</div>
+      <div>asdfsdfa</div>
+      <div>asdfsdfa</div>
+      <div>asdfsdfa</div>
+      <div>asdfsdfa</div>
+      <div>asdfsdfa</div>
+      <div>asdfsdfa</div>
+      <div>asdfsdfa</div>
+    </>
+  );
+}
