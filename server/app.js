8fd1b531d0764761fdc0aaed83fcbe140d67d5dd
Joel Cheah <gjoelcheah@gmail.com>
2024-01-12 11:59:50 +0000

    add dotenv to manage env vars

--- server/app.js
+++ server/app.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const inventoryRouter = require('./routers/inventory.router');
 const db = require('./models/index');
+const inventoryRouter = require('./routers/inventory.router');
+const ingredientRouter = require('./routers/ingredient.router.js')
 
 const app = express();
 app.use(cors());
@@ -13,22 +15,21 @@ app.use((req, res, next) => {
 });
 // Example route setup
 app.use('/inventory', inventoryRouter);
+app.use('/ingredient', ingredientRouter);
 
-// // Database connection
-// sequelize
-//   .authenticate()
-//   .then(() => console.log('Database connected...'))
-//   .catch((err) => console.log('Error: ' + err));
+
+
 
 (async () => {
-    try {
-      await db.sequelize.sync();
-      console.log('Database synced successfully.');
-      const PORT = process.env.PORT || 3000;
-      app.listen(PORT, console.log(`Server started on port ${PORT}`));
-    } catch (error) {
-      console.error('Unable to sync the database:', error);
-      // Handle error appropriately - maybe exit process
-      process.exit(1);
-    }
-  })();
+  try {
+    // await db.sequelize.drop();
+    await db.sequelize.sync();
+    console.log('Database synced successfully.');
+    const PORT = process.env.PORT || 3001;
+    app.listen(PORT, console.log(`Server started on port ${PORT}`));
+  } catch (error) {
+    console.error('Unable to sync the database:', error);
+    // Handle error appropriately - maybe exit process
+    process.exit(1);
+  }
+})();
--- server/models/index.js
+++ server/models/index.js
@@ -4,17 +4,25 @@ const path = require('path');
 const db = {};
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('pantrypal', 'postgres', '', {
-  host: 'localhost',
-  // logging: false,
-  dialect: 'postgres'
-});
+const sequelize = new Sequelize(
+  process.env.PG_DB,
+  process.env.PG_USER,
+  process.env.PG_PASSWORD,
+  {
+    host:  process.env.PG_HOST,
+    logging: false,
+    dialect: 'postgres',
+  }
+);
 
 const files = fs.readdirSync(__dirname);
 
 for (let file of files) {
   if (file !== 'index.js') {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    );
     db[model.name] = model;
   }
 }
