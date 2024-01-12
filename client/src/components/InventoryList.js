17537e83e96518205789d707c6e52f7806621e81
Joel Cheah <gjoelcheah@gmail.com>
2024-01-12 15:42:06 +0000

    Style inventory editor

--- client/src/components/InventoryList.js
+++ client/src/components/InventoryList.js
@@ -1,16 +1,43 @@
 'use client';
 import { useAppSelector } from '@/lib/hooks';
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  IconButton,
+  Typography,
+} from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export default function InventoryList() {
   const inventory = useAppSelector((state) => state.inventory);
 
-  
+  const handleDelete = (itemId) => {
+    // Implement the delete functionality
+    console.log('Delete item with id:', itemId);
+  };
+
   const renderedInventory = inventory.map((item) => (
-    <li key={item.id}>{item.name}</li>
+    <Card key={item.id} sx={{ display: 'flex', my: 2 }}>
+      {/* <CardMedia
+        component="img"
+        sx={{ width: '20%' }}
+        image={item.image} // Assuming 'image' is the URL to the item's image
+        alt={item.name}
+      /> */}
+      <CardContent sx={{ flex: '1 0 auto' }}>
+        <Typography component="div" variant="p">
+          {item.name}
+        </Typography>
+      </CardContent>
+      <IconButton
+        onClick={() => handleDelete(item.id)}
+        aria-label="delete"
+        sx={{ alignSelf: 'center' }}>
+        <DeleteIcon />
+      </IconButton>
+    </Card>
   ));
-  return (
-    <>
-      <ul>{renderedInventory}</ul>
-    </>
-  );
+
+  return <div>{renderedInventory}</div>;
 }
--- client/src/components/features/inventory/InventoryEditor.js
+++ client/src/components/features/inventory/InventoryEditor.js
@@ -3,6 +3,7 @@ import IngredientSelect from '@/components/IngredientSelect';
 import InventoryList from '@/components/InventoryList';
 import { inventoryAdded } from '@/lib/features/inventory/inventorySlice';
 import { useAppDispatch } from '@/lib/hooks';
+import { Box } from '@mui/material';
 import { useState } from 'react';
 
 export default function InventoryEditor() {
@@ -11,15 +12,15 @@ export default function InventoryEditor() {
 
   function onIngredientSelect(event, ingredient) {
     console.log(ingredient);
-    dispatch(inventoryAdded(ingredient));
+    // dispatch(inventoryAdded(ingredient));
     setAutocompleteKey((prev) => prev + 1);
   }
   return (
-    <>
+    <Box padding={2}>
       <IngredientSelect
         onChange={onIngredientSelect}
         key={autocompleteKey}></IngredientSelect>
       <InventoryList></InventoryList>
-    </>
+    </Box>
   );
 }
