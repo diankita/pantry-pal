072c41267d8ab54f7cf965fb4f8a885cb27a5547
Joel Cheah <gjoelcheah@gmail.com>
2024-01-13 10:44:01 +0000

    complete api for get list of inventory

--- server/controllers/inventory.controller.js
+++ server/controllers/inventory.controller.js
@@ -1,7 +1,6 @@
 const db = require('../models');
 
 exports.addToInventory = async (req, res) => {
-
   // Start a transaction
   const transaction = await db.sequelize.transaction();
   try {
@@ -44,6 +43,33 @@ exports.addToInventory = async (req, res) => {
   }
 };
 
+// TODO do i use authentication to see which user's inventory i am getting or do they just authenticate and pass userId to me. will it be misused and an authenticated user passes another user's id to me.
 exports.getAllInventory = async (req, res) => {
-  console.log('asldfas');
+  try {
+    const userId = req.query.userId;
+
+    // Authenticate user (this can be done via middleware)
+    // ...
+
+    // Fetch all inventory items for the given user
+    const inventoryItems = await db.Inventory.findAll({
+      where: { UserId: userId },
+      include: [
+        {
+          model: db.Ingredient,
+          as: 'Ingredient', // Assuming 'Ingredient' is the alias in Inventory model
+          attributes: ['id', 'name', 'image', 'aisle'],
+        },
+      ],
+    });
+
+    // if (inventoryItems.length === 0) {
+    //   return res.status(404).send('No inventory items found for this user');
+    // }
+
+    res.status(200).json(inventoryItems.map((item) => item.Ingredient));
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error retrieving inventory');
+  }
 };
