3f7ebbca1701a54b6a31a61c5f459ca948669baa
Joel Cheah <gjoelcheah@gmail.com>
2024-01-13 17:01:22 +0000

    Fix styling for add to inventory

--- client/src/app/(main-group)/inventory/page.js
+++ client/src/app/(main-group)/inventory/page.js
@@ -1,17 +1,29 @@
 import IngredientSelect from '@/components/IngredientSelect';
 import TopNav from '@/components/TopNav';
 import InventoryList from '@/components/InventoryList';
-import { AppBar, Autocomplete, TextField, Toolbar } from '@mui/material';
+import {
+  AppBar,
+  Autocomplete,
+  Box,
+  Button,
+  TextField,
+  Toolbar,
+} from '@mui/material';
 import InventoryEditor from '@/components/features/inventory/InventoryEditor';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 export default function Page() {
   return (
-    <div id="inventory-container">
-      <TopNav title="Cook with Pantry"></TopNav>
-
-      <InventoryEditor></InventoryEditor>
-
-      {/* List of ingredients selected */}
-    </div>
+    <Box height="100%" paddingX={1} paddingTop={2}>
+      <Box overflow="auto" height="calc(100% - 100px)">
+        <InventoryEditor></InventoryEditor>
+      </Box>
+      <Button
+        variant="contained"
+        href="/inventory/recipe-suggestion"
+        fullWidth="true" endIcon={<ArrowForwardIcon />}>
+        View Recipes
+      </Button>
+    </Box>
   );
 }
