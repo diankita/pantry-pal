5a1bc4492eb6838fe484386447eb334da029cbe9
Joel Cheah <gjoelcheah@gmail.com>
2024-01-13 10:45:55 +0000

    Add todo for backend

--- server/app.js
+++ server/app.js
@@ -17,6 +17,15 @@ app.use((req, res, next) => {
 app.use('/inventory', inventoryRouter);
 app.use('/ingredient', ingredientRouter);
 
+// TODO unified error handling
+// app.use((err, req, res, next) => {
+//   return res.status(500).json({
+//     status: 'error',
+//     message: err.message,
+//     path: req.path
+//   });
+// });
+
 (async () => {
   try {
     await db.sequelize.drop();
