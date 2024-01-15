8663cb0014efc641cccc3055a24e7d04c82b44c2
Joel Cheah <gjoelcheah@gmail.com>
2024-01-15 08:27:53 +0000

    Change to use nextjs link

--- client/src/app/(main-group)/inventory/page.js
+++ client/src/app/(main-group)/inventory/page.js
@@ -1,6 +1,6 @@
 import InventoryEditor from '@/components/features/inventory/InventoryEditor';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-
+import Link from 'next/link';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 
@@ -10,13 +10,17 @@ export default function Page() {
       <Box overflow="auto" height="calc(100% - 100px)">
         <InventoryEditor></InventoryEditor>
       </Box>
-      <Button
-        variant="contained"
-        href="/inventory/recipe-suggestion"
-        fullWidth={true}
-        endIcon={<ArrowForwardIcon />}>
-        View Recipes
-      </Button>
+      <Link
+        href={{
+          pathname: '/inventory/recipe-suggestion',
+        }}>
+        <Button
+          variant="contained"
+          fullWidth={true}
+          endIcon={<ArrowForwardIcon />}>
+          View Recipes
+        </Button>
+      </Link>
     </Box>
   );
 }
