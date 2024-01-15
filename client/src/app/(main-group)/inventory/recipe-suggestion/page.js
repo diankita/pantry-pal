dc7998a492e2af9df8183de03aeda39843a54bbb
Joel Cheah <gjoelcheah@gmail.com>
2024-01-15 16:50:11 +0000

    Use mui container for recipe list

--- client/src/app/(main-group)/inventory/recipe-suggestion/page.js
+++ client/src/app/(main-group)/inventory/recipe-suggestion/page.js
@@ -1,9 +1,10 @@
 import RecipeList from '@/components/features/recipe/RecipeList';
+import Box from '@mui/material/Box';
 
 export default function Page() {
   return (
-    <div>
+    <Box overflow="auto" height="calc(100% - 56px)">
       <RecipeList></RecipeList>
-    </div>
+    </Box>
   );
 }
