da7e51d4612d73454ded0bf923ed0194b1b2f530
Joel Cheah <gjoelcheah@gmail.com>
2024-01-16 00:51:22 +0000

    Make layout suitable for desktop

--- client/src/app/(main-group)/layout.js
+++ client/src/app/(main-group)/layout.js
@@ -1,12 +1,19 @@
+'use client'
 import BottomNav from '@/components/BottomNav';
 import TopNav from '@/components/TopNav';
 import Box from '@mui/material/Box';
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 export default function MainLayout({ children }) {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
   return (
     <Box height="100%" paddingBottom={7}>
       <TopNav title="aslkdj"></TopNav>
-      {children}
+      <Box width={isMobile ? '100%' : '40vw'} marginX={'auto'} height={'100%'}>
+        {children}
+      </Box>
       <BottomNav></BottomNav>
     </Box>
     // TODO use redux to control state for showing bottom nav
